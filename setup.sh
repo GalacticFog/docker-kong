973d07d32af5b9a6008e7aa7b23681b6c443fc1d
Brad Futch <brad@galacticfog.com>
2017-04-06 12:56:38 -0500

    updating the setup script to create the kong db if it does not exist

--- setup.sh
+++ setup.sh
@@ -11,3 +11,24 @@ postgres:
 EOM
 
 echo "$DATABASE" >> /etc/kong/kong.yml
+
+__create_db() {
+
+psql -h $POSTGRES_HOSTNAME -p $POSTGRES_PORT -U $POSTGRES_USER -v ON_ERROR_STOP=1 <<-EOSQL
+    CREATE DATABASE "$POSTGRES_NAME";
+    GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_NAME" TO "$POSTGRES_USER";
+EOSQL
+}
+
+# create a .pgpass file which is used for psql
+echo "$POSTGRES_HOSTNAME:$POSTGRES_PORT:*:$POSTGRES_USER:$POSTGRES_PASSWORD \n" >> ~/.pgpass
+
+# check that our database exists
+psql -h $POSTGRES_HOSTNAME -p $POSTGRES_PORT -U $POSTGRES_USER -lqt | cut -d \| -f 1 | grep -qw $POSTGRES_NAME
+
+if [[ "$?" -eq "0" ]]; then
+   echo "database exists";
+else
+  echo "database does not exist, creating..."
+  __create_db $POSTGRES_NAME
+fi
